2c17bec214ec0eac4fb5d97b4f0422c672f02523
erik1997 <erikriosetiawan49@gmail.com>
2019-03-12 21:50:17 +0700

    New Project

--- Praktik/HitungUmur/src/com/erikriosetiawan/HitungUmur.java
+++ Praktik/HitungUmur/src/com/erikriosetiawan/HitungUmur.java
@@ -1,3 +1,59 @@
+package com.erikriosetiawan;
+import java.util.Scanner;
+import java.util.Calendar;
+
 public class HitungUmur {
+
+    // Membuat Objek dari Library Java yang Telah di Import
+    private static Scanner userInput = new Scanner(System.in);
+    private static Calendar calendar = Calendar.getInstance();
+    private static int thisYear = calendar.getWeekYear();
+
+    public static void main(String[] args) {
+
+        //Memanggil Method tamilkanData()
+        tampilkanData();
+    }
+
+    // Membuat Method Print
+    private static void print(String str) {
+        System.out.print(str);
+    }
+
+    // Membuat Method Input Tahun Lahir User
+    private static int inputTahunLahirUser() {
+        int tahunLahir;
+        print("Masukkan Tahun Lahir Anda : ");
+        tahunLahir = userInput.nextInt();
+        return tahunLahir;
+    }
+
+    // Membuat Method Input Nama User
+
+    private static String inputNamaUser(){
+        String namaUser;
+        print("Masukkan Nama Anda : ");
+        namaUser = userInput.nextLine();
+        return namaUser;
+    }
+
+    // Membuat Method Proses Data Umur User
+    private static int prosesDataUmur() {
+        int umurUser, tahunLahir;
+        tahunLahir = inputTahunLahirUser();
+        umurUser = thisYear - tahunLahir;
+        return umurUser;
+    }
+
+    // Membuat Method Untuk Menamilkan Data
+    private static void tampilkanData() {
+        String namaUser;
+        int umurUser;
+        namaUser = inputNamaUser();
+        umurUser = prosesDataUmur();
+
+        print("Halo " + namaUser + "! Umur Anda adalah " + umurUser + " tahun\n");
+        print("Segera tobat ya Broo! ;)");
+    }
 
 }
